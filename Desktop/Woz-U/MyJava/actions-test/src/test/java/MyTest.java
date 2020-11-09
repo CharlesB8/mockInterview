8270ee37edade1c08a506669a7543e8133f08361
Charles Bartels <charlieb2898@gmail.com>
2020-11-09 09:43:29 -0600

    added test

---
+++ Desktop/Woz-U/MyJava/actions-test/src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MyTest {
+
+    @Test
+    public void test(){
+       assertEquals(5,5);
+    }
+}
